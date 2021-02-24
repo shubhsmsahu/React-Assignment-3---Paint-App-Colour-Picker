694abf8670c85c141f4691d03e762ce4bc78cc6b
shubhamsahu <sahu4816@gmail.com>
2021-02-24 16:00:12 +0530

    newcommitComplete

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,19 @@
+import React, { useState } from "react";
 
+const Selection = (props) => {
+  const [SelectionStyle, updateSelectionStyle] = useState({ background: "" });
+  const { applyColor, id } = props;
+  return (
+    <div
+      className="fix-box"
+      style={SelectionStyle}
+      onClick={() => {
+        applyColor(updateSelectionStyle);
+      }}
+    >
+      <h2>{id}</h2>
+    </div>
+  );
+};
+
+export default Selection;
